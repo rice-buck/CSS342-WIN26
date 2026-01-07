652c48b487933698b4e037c1ed1faa821920cc41
Rhys Zachary Buckeye <rbucke@csslab15.uwb.edu>
2026-01-06 20:07:17 -0800

    First commit

---
+++ Program1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ImageLib.h"
+#include <string>
+
+//string for filename
+const std::string fileName = "test1.gif";
+
+int main(){
+    image input = READGIF(fileName);
+    //check if image is valid 
+    if
+    int test = test;
+    std::
+}
